e65abfcff5553683a5914587fa4fbda10f590033
Antonio Cruz Hernandez <andejecruher@gmail.com>
2023-07-25 23:02:21 -0600

    Create section portafolio

---
+++ src/views/pages/portafolio/Portafolio.vue
@@ -0,0 +1,158 @@
+<script setup>
+import avatar1 from "@images/avatars/avatar-1.png";
+import avatar2 from "@images/avatars/avatar-2.png";
+import avatar3 from "@images/avatars/avatar-3.png";
+import avatar4 from "@images/avatars/avatar-4.png";
+import pages2 from "@images/pages/2.png";
+
+const avatars = [avatar1, avatar2, avatar3, avatar4];
+
+const navigationTab = ref("Frontend");
+
+const tabItems = ["Todos","Frontend", "Backend", "Full Stack", "Mobile"];
+</script>
+<template>
+  <!-- 👉 Transactions -->
+  <VCol cols="12" md="12" sm="12" order="3">
+    <VCard>
+      <VCardItem>
+        <VCardTitle>Portafolio</VCardTitle>
+      </VCardItem>
+      <VTabs v-model="navigationTab">
+        <VTab v-for="item in tabItems" :key="item" :value="item">
+          {{ item }}
+        </VTab>
+      </VTabs>
+
+      <VDivider />
+
+      <!-- tabs content -->
+      <VWindow v-model="navigationTab">
+        <VWindowItem v-for="item in tabItems" :key="item" :value="item">
+          <VCardText>
+            <v-row>
+              <v-col cols="12" md="6" xs="6">
+                <VCard>
+                  <VImg :src="pages2" size="250" />
+
+                  <VCardText class="position-relative">
+                    <!-- User Avatar -->
+                    <VAvatar size="75" class="avatar-center" :image="avatar1" />
+
+                    <!-- Title, Subtitle & Action Button -->
+                    <div class="d-flex justify-space-between flex-wrap pt-8">
+                      <div class="me-2 mb-2">
+                        <VCardTitle class="pa-0"> Robert Meyer </VCardTitle>
+                        <VCardSubtitle class="text-caption pa-0">
+                          London, UK
+                        </VCardSubtitle>
+                      </div>
+                      <VBtn>send request</VBtn>
+                    </div>
+
+                    <!--  Mutual Friends -->
+                    <div class="d-flex justify-space-between align-center mt-4">
+                      <span class="font-weight-medium">18 mutual friends</span>
+
+                      <div class="v-avatar-group">
+                        <VAvatar
+                          v-for="avatar in avatars"
+                          :key="avatar"
+                          :image="avatar"
+                          size="40"
+                        />
+                      </div>
+                    </div>
+                  </VCardText>
+                </VCard>
+              </v-col>
+              <v-col cols="12" md="6" xs="6">
+                <VCard>
+                  <VImg :src="pages2" />
+
+                  <VCardText class="position-relative">
+                    <!-- User Avatar -->
+                    <VAvatar size="75" class="avatar-center" :image="avatar1" />
+
+                    <!-- Title, Subtitle & Action Button -->
+                    <div class="d-flex justify-space-between flex-wrap pt-8">
+                      <div class="me-2 mb-2">
+                        <VCardTitle class="pa-0"> Robert Meyer </VCardTitle>
+                        <VCardSubtitle class="text-caption pa-0">
+                          London, UK
+                        </VCardSubtitle>
+                      </div>
+                      <VBtn>send request</VBtn>
+                    </div>
+
+                    <!--  Mutual Friends -->
+                    <div class="d-flex justify-space-between align-center mt-4">
+                      <span class="font-weight-medium">18 mutual friends</span>
+
+                      <div class="v-avatar-group">
+                        <VAvatar
+                          v-for="avatar in avatars"
+                          :key="avatar"
+                          :image="avatar"
+                          size="40"
+                        />
+                      </div>
+                    </div>
+                  </VCardText>
+                </VCard>
+              </v-col>
+            </v-row>
+          </VCardText>
+
+          <VCardText>
+            <div class="text-center">
+              <v-pagination
+                v-model="page"
+                :length="4"
+                prev-icon="mdi-menu-left"
+                next-icon="mdi-menu-right"
+              ></v-pagination>
+            </div>
+          </VCardText>
+        </VWindowItem>
+      </VWindow>
+    </VCard>
+  </VCol>
+</template>
+
+<script>
+import CardNavigation from "../cards/card-basic/CardNavigation.vue";
+export default {
+  components: {
+    CardNavigation,
+  },
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  methods: {},
+  computed: {},
+  mounted() {},
+};
+</script>
+<style lang="scss" scoped>
+.avatar-center {
+  position: absolute;
+  border: 3px solid rgb(var(--v-theme-surface));
+  inset-block-start: -2rem;
+  inset-inline-start: 1rem;
+}
+
+// membership pricing
+.member-pricing-bg {
+  position: relative;
+  background-color: rgba(var(--v-theme-on-surface), var(--v-hover-opacity));
+}
+
+.membership-pricing {
+  sup {
+    inset-block-start: 9px;
+  }
+}
+</style>
