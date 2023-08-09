6679f2b99f9a75e7e22f8271115857d38fc8e488
Antonio Cruz Hernandez <andejecruher@gmail.com>
2023-08-08 21:03:02 -0600

    update en router rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,77 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: '/inicio' },
+    { path: "/", redirect: "/inicio" },
     {
-      path: '/',
-      component: () => import('../layouts/default.vue'),
+      path: "/",
+      component: () => import("../layouts/default.vue"),
       children: [
         {
-          path: 'inicio',
-          component: () => import('../views/pages/home/Home.vue'),
+          path: "inicio",
+          component: () => import("../views/pages/home/Home.vue"),
         },
 
         // {
         //   path: 'portafolio',
         //   component: () => import('../views/pages/portafolio/Portafolio.vue'),
         // },
-        // {
-        //   path: 'blog',
-        //   component: () => import('../views/pages/blog/Blog.vue'),
-        // },
-        // {
-        //   path: 'blog/:id',
-        //   component: () => import('../views/pages/blog/Post.vue'),
-        // },
         {
-          path: 'contacto',
-          component: () => import('../views/pages/contact/Contact.vue'),
+          path: "blog",
+          component: () => import("../views/pages/blog/Blog.vue"),
+        },
+        {
+          path: "blog/:id",
+          component: () => import("../views/pages/blog/Post.vue"),
+        },
+        {
+          path: "contacto",
+          component: () => import("../views/pages/contact/Contact.vue"),
         },
       ],
     },
     {
-      path: '/',
-      component: () => import('../layouts/blank.vue'),
+      path: "/",
+      component: () => import("../layouts/blank.vue"),
       children: [
         {
-          path: 'login',
-          component: () => import('../pages/login.vue'),
+          path: "login",
+          component: () => import("../pages/login.vue"),
         },
         {
-          path: 'register',
-          component: () => import('../pages/register.vue'),
+          path: "register",
+          component: () => import("../pages/register.vue"),
         },
         {
-          path: 'forgot-password',
-          component: () => import('../pages/forgot-password.vue'),
+          path: "forgot-password",
+          component: () => import("../pages/forgot-password.vue"),
         },
         {
-          path: '/:pathMatch(.*)*',
-          component: () => import('../pages/[...all].vue'),
+          path: "/:pathMatch(.*)*",
+          component: () => import("../pages/[...all].vue"),
         },
       ],
     },
     {
-      path: '/dashboard',
-      component: () => import('../layouts/dashboard.vue'),
+      path: "/dashboard",
+      component: () => import("../layouts/dashboard.vue"),
       children: [
         {
-          path: 'usuarios',
-          component: () => import('../views/pages/usuarios/Usuarios.vue'),
+          path: "usuarios",
+          component: () => import("../views/pages/usuarios/Usuarios.vue"),
         },
         {
-          path: 'categorias',
-          component: () => import('../pages/typography.vue'),
+          path: "categorias",
+          component: () => import("../pages/typography.vue"),
         },
         {
-          path: 'articulos',
-          component: () => import('../pages/typography.vue'),
+          path: "articulos",
+          component: () => import("../pages/typography.vue"),
         },
       ],
     },
   ],
-})
+});
 
-export default router
+export default router;
